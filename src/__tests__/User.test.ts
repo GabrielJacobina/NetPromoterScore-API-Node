95f6d3b8507e8c23e3e4d3abab182a40d5ced569
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-02-26 22:22:05 -0300

    Creating survey tests

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -12,11 +12,21 @@ describe("Users", () => {
 
     it("Should be able to create a new user", async () => {
         const response = await request(app).post("/users")
-    .send({
-        email: "user@example.com",
-        name: "User Example"
+            .send({
+                email: "user@example.com",
+                name: "User Example"
+            });
+
+        expect(response.status).toBe(201);
     });
 
-    expect(response.status).toBe(201);
-    });
+    it("Sould not be able to create a user with exists email", async () => {
+        const response = await request(app).post("/users")
+            .send({
+                email: "user@example.com",
+                name: "User Example"
+            });
+
+        expect(response.status).toBe(400);
+    })
 });
