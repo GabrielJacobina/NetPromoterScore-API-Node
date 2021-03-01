74838b62e152a5bbfd2d6d43af059eb6834693bf
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-02-28 21:11:52 -0300

    Changing select from or to and

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -43,7 +43,7 @@ class SendMailController {
         }
 
         const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{user_id: user.id}, {value: null}],
+            where: {user_id: user.id, value: null},
             relations: ["user", "survey"]
         });
 
