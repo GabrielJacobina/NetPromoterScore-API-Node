c1a161d623cbb7a83869e81a6a193bf77e4553bb
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-02-28 22:21:56 -0300

    Add drop Trable at the end of the tests

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -1,4 +1,4 @@
-import { response } from 'express';
+import { getConnection } from 'typeorm';
 import request from 'supertest';
 import { app } from '../app';
 
@@ -9,6 +9,12 @@ describe("Surveys", () => {
     beforeAll(async () => {
         const connection = await createConnection();
         await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        const connection = getConnection();
+        await connection.dropDatabase();
+        await connection.close();
     });
 
     it("Should be able to create a new survey", async () => {
