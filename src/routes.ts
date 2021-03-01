966cc85481ba815ab168e3f0f2684e05a6eb298a
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-02-28 21:37:35 -0300

    Creating AnswerController

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+
 
 const router = Router();
 
@@ -10,12 +12,16 @@ const surveysController = new SurveysController();
 
 const sendMailController = new SendMailController();
 
+const answerController = new AnswerController();
+
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 
 export { router };
